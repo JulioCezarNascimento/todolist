7fef421aab8e52e4b5b6f5d38421a09f5a6a367f
JulioCezarNascimento <silvajuliocezar992@gmail.com>
2023-10-13 09:52:00 -0300

    Validação e conexão com o banco de dados.

--- src/main/java/br/com/juliocezar/todolist/user/UserControler.java
+++ src/main/java/br/com/juliocezar/todolist/user/UserControler.java
@@ -1,5 +1,8 @@
 package br.com.juliocezar.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserControler {
     
-    @PostMapping("/")
-    public void createUser(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping
+    public UserModel create(@RequestBody UserModel userModel) {
+      var usercreated = this.userRepository.save(userModel);
+      return usercreated;
     }
-}
+    }
+
--- src/main/java/br/com/juliocezar/todolist/user/UserModel.java
+++ src/main/java/br/com/juliocezar/todolist/user/UserModel.java
@@ -1,8 +1,31 @@
 package br.com.juliocezar.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_user")
 public class UserModel {
 
-   public String name;
-   public String email;
-   public String password;
+   @Id
+   @GeneratedValue(generator = "UUID")
+   private UUID id;
+
+   @Column(unique = true)
+   private String name;
+   private String email;
+   private String password;
+
+   @CreationTimestamp
+   private LocalDateTime createdAt;
+
 }
